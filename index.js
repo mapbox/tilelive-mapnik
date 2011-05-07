10b44314965e3a4d47e9cefe1eb76323341f5b96
Young Hahn <young@developmentseed.org>
2011-05-06 21:11:49 -0400

    Initial commit of tilelive-mapnik.

---
+++ index.js
@@ -0,0 +1,23 @@
+var Map = require('./lib/map');
+
+module.exports = {
+    pool: function(datasource, options) {
+        return {
+            create: function(callback) {
+                console.log(datasource);
+                var resource = new Map(datasource, options);
+                resource.initialize(function(err) {
+                    if (err) throw err;
+                    callback(resource);
+                });
+            },
+            destroy: function(resource) {
+                resource.destroy();
+            }
+        };
+    },
+    serve: function(resource, options, callback) {
+        resource.render(options, callback);
+    }
+};
+
