d6e25ada89defb6d91cf0beec8a7246a42724075
Konstantin Käfer <github@kkaefer.com>
2011-07-18 21:52:14 +0200

    test with JSON mml file passed in as uri.data

--- test/render.test.js
+++ test/render.test.js
@@ -30,34 +30,42 @@ assert.imageEqualsFile = function(buffer, file_b, callback) {
     });
 };
 
+var tileCoords = [
+    [0, 0, 0],
+    [1, 0, 0],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 1, 1],
+    [2, 0, 0],
+    [2, 0, 1],
+    [2, 0, 2],
+    [2, 0, 3],
+    [2, 1, 0],
+    [2, 1, 1],
+    [2, 1, 2],
+    [2, 1, 3],
+    [2, 2, 0],
+    [2, 2, 1],
+    [2, 2, 2],
+    [2, 2, 3],
+    [2, 3, 0],
+    [2, 3, 1],
+    [2, 3, 2],
+    [2, 3, 3]
+];
+
+var tileCoordsCompletion = {};
+tileCoords.forEach(function(coords) {
+    tileCoordsCompletion['tile_' + coords[0] + '_' + coords[1] + '_' + coords[2]] = true;
+});
+
 
 exports['getTile()'] = function(beforeExit) {
     var completion = {};
     new mapnik('mapnik://./test/data/world.mml', function(err, source) {
         if (err) throw err;
 
-        [   [0, 0, 0],
-            [1, 0, 0],
-            [1, 0, 1],
-            [1, 1, 0],
-            [1, 1, 1],
-            [2, 0, 0],
-            [2, 0, 1],
-            [2, 0, 2],
-            [2, 0, 3],
-            [2, 1, 0],
-            [2, 1, 1],
-            [2, 1, 2],
-            [2, 1, 3],
-            [2, 2, 0],
-            [2, 2, 1],
-            [2, 2, 2],
-            [2, 2, 3],
-            [2, 3, 0],
-            [2, 3, 1],
-            [2, 3, 2],
-            [2, 3, 3]
-        ].forEach(function(coords) {
+        tileCoords.forEach(function(coords) {
             source.getTile(coords[0], coords[1], coords[2], function(err, tile, headers) {
                 if (err) throw err;
                 var key = coords[0] + '_' + coords[1] + '_' + coords[2];
@@ -73,29 +81,39 @@ exports['getTile()'] = function(beforeExit) {
     });
 
     beforeExit(function() {
-        assert.deepEqual(completion, {
-            tile_2_0_2: true,
-            tile_2_0_1: true,
-            tile_2_0_0: true,
-            tile_1_1_0: true,
-            tile_1_1_1: true,
-            tile_1_0_1: true,
-            tile_0_0_0: true,
-            tile_1_0_0: true,
-            tile_2_3_2: true,
-            tile_2_3_0: true,
-            tile_2_2_2: true,
-            tile_2_2_3: true,
-            tile_2_2_1: true,
-            tile_2_2_0: true,
-            tile_2_1_2: true,
-            tile_2_1_3: true,
-            tile_2_1_1: true,
-            tile_2_1_0: true,
-            tile_2_0_3: true,
-            tile_2_3_1: true,
-            tile_2_3_3: true
+        assert.deepEqual(completion, tileCoordsCompletion);
+    });
+};
+
+
+exports['getTile() with JSON mml file'] = function(beforeExit) {
+    var completion = {};
+    var mml = JSON.parse(fs.readFileSync('./test/data/world.mml', 'utf8'));
+    var uri = {
+        protocol: 'mapnik:',
+        data: mml
+    };
+
+    new mapnik(uri, function(err, source) {
+        if (err) throw err;
+
+        tileCoords.forEach(function(coords) {
+            source.getTile(coords[0], coords[1], coords[2], function(err, tile, headers) {
+                if (err) throw err;
+                var key = coords[0] + '_' + coords[1] + '_' + coords[2];
+                assert.imageEqualsFile(tile, 'test/fixture/tiles/' + key + '.png', function(err, similarity) {
+                    completion['tile_' + key] = true;
+                    if (err) throw err;
+                    assert.deepEqual(headers, {
+                        "Content-Type": "image/png"
+                    });
+                });
+            });
         });
+    });
+
+    beforeExit(function() {
+        assert.deepEqual(completion, tileCoordsCompletion);
     });
 };
 
@@ -111,4 +129,4 @@ exports['getTile() with invalid style'] = function(beforeExit) {
     beforeExit(function() {
         assert.ok(completion);
     })
-}
+};
