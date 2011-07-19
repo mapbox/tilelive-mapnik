1361380c19596bdd7a269f76de491d8864f6a1d0
Konstantin Käfer <github@kkaefer.com>
2011-07-19 12:20:24 +0200

    move assert.imageEqualsFile to its own file

--- test/render.test.js
+++ test/render.test.js
@@ -1,34 +1,6 @@
 var fs = require('fs');
-var assert = require('assert');
+var assert = require('./support/assert');
 var mapnik = require('..');
-
-assert.imageEqualsFile = function(buffer, file_b, callback) {
-    if (!callback) callback = function(err) { if (err) throw err; };
-    file_b = require('path').resolve(file_b);
-    var file_a = '/tmp/' + (Math.random() * 1e16);
-    var err = require('fs').writeFileSync(file_a, buffer);
-    if (err) throw err;
-
-    require('child_process').exec('compare -metric PSNR "' + file_a + '" "' +
-            file_b + '" /dev/null', function(err, stdout, stderr) {
-        if (err) {
-            require('fs').unlinkSync(file_a);
-            callback(err);
-        } else {
-            stderr = stderr.trim();
-            if (stderr === 'inf') {
-                require('fs').unlinkSync(file_a);
-                callback(null);
-            } else {
-                var similarity = parseFloat(stderr);
-                var err = new Error('Images not equal(' + similarity + '): ' +
-                        file_a  + '    ' + file_b);
-                err.similarity = similarity;
-                callback(err);
-            }
-        }
-    });
-};
 
 var tileCoords = [
     [0, 0, 0],
